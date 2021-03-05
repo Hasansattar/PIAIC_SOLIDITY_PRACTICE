b42b6feef4af25c2044403a0be2604767fc1e852
Hasansattar <hasansattar650@gmail.com>
2021-03-06 02:20:53 +0500

    assigment 2

---
+++ assigment2/certification.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.0;
+
+
+contract Certification{
+    
+    
+    
+    
+    
+    function getContractAddress()public view returns(address)
+   { return address(this);}
+   
+    function getContractBalance()public view returns(uint)
+   {    return address(this).balance;  }
+   
+   
+   
+    
+   uint public balance = 0;
+   
+   function receivepayment() public payable   {
+     balance += msg.value;
+     
+     
+     if(balance == 10000000000000000000 wei){
+         destroyContract();
+     }
+     
+       
+   }
+ 
+
+   
+   function destroyContract()public {
+        address payable addr = payable(address(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB));
+       selfdestruct(addr);
+   
+       
+       
+   }
+   
+
+    
+}
